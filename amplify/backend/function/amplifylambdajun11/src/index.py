741d47514f9d04af9db7f55257bf84b0e5e596df
Micah <contact_micah@yahoo.com>
2024-06-11 07:51:46 -0700

    amplify add api + lambda

---
+++ amplify/backend/function/amplifylambdajun11/src/index.py
@@ -0,0 +1,31 @@
+import json
+'''
+def handler(event, context):
+  print('received event:')
+  print(event)
+  
+  return {
+      'statusCode': 200,
+      'headers': {
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      },
+      'body': json.dumps('Hello from your new Amplify Python lambda!')
+  }
+'''  
+  
+def customer_handler(event, context):
+  print(event)
+  customerId = event['pathParameters']['customerId']  # API: /customers/123 => 123 becomes customerId
+  customer = {'customerId': customerId, 'customerName': "Customer " + customerId}
+  response = {
+		'statusCode': 200,
+		# Uncomment below to enable CORS requests
+		'headers': {
+		    'Access-Control-Allow-Origin': '*',
+		    'Access-Control-Allow-Headers': '*'
+		},
+		'body': json.dumps(customer)
+    }
+  return response  
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myRestApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifylambdajun11": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
